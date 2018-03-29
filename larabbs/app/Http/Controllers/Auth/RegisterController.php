fa9cb48b4b628fecb63beb3222d1ff1c80faf4fc
lihanlu <lihanlu123@gmail.com>
2018-03-29 18:11:50 +0800

    Basic user profile editing

--- larabbs/app/Http/Controllers/Auth/RegisterController.php
+++ larabbs/app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -46,7 +46,7 @@ class RegisterController extends Controller
      * @return \Illuminate\Contracts\Validation\Validator
      */
 
-     
+
      protected function validator(array $data)
      {
          return Validator::make($data, [
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return App\Models\User
      */
     protected function create(array $data)
     {
