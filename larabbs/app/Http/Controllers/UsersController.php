2c1c153ed4adf2a237a4b6888e1046ea0620b24e
lihanlu <lihanlu123@gmail.com>
2018-03-30 17:56:43 +0800

    Only authorized user can edit user profile

--- larabbs/app/Http/Controllers/UsersController.php
+++ larabbs/app/Http/Controllers/UsersController.php
@@ -5,21 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+  public function __construct()
+  {
+      $this->middleware('auth', ['except' => ['show']]);
+  }
+
+
     public function show(User $user)
   {
     return view('users.show', compact('user'));
   }
-   public function edit(User $user)
-   {
-     return view('users.edit',compact('user'));
 
-   }
-   public function update(Request $request,User $user)
-   {
-     $user->update($request->all());
-     return redirect()->route('users.show',$user->id)->with('succes','編輯成功');
-   }
+   public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+   public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
+    {
+        
+
+        $this->authorize('update', $user);
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
 }
